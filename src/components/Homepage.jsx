35c52468c361254ce7dd7169d1b9293b985cdfd6
Wojtek <mitiolf@outlook.com>
2022-03-23 12:55:25 +0100

    qwe

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,9 +6,9 @@ import programer from '../img/programer.png'
 const Homepage = () => {
   return (
     <div className='homepage-container'>
-        <h1>Hi, I'm Wojtek</h1>
-        <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2>Programer</h2></Link></div>
-        <div className='artist-container'><Link to="artist"><img src={artist} alt='artist graphic'/><h2>Artist</h2></Link></div>
+        <h1 className='homepage-h1'>Hi, I'm Wojtek</h1>
+        <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2 className='homepage-h2'>Programer</h2></Link></div>
+        <div className='artist-container'><Link to="artist"><img src={artist} alt='artist graphic'/><h2 className='homepage-h2'>Artist</h2></Link></div>
     </div>
   )
 }
