e7a12595648eebe057a2db2a0d004726b895a7a8
Wojtek <mitiolf@outlook.com>
2022-03-23 12:35:05 +0100

    First commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import artist from '../img/artist.png'
+import programer from '../img/programer.png'
+
+const Homepage = () => {
+  return (
+    <div className='homepage-container'>
+        <h1>Hi, I'm Wojtek</h1>
+        <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2>Programer</h2></Link></div>
+        <div className='artist-container'><Link to="artist"><img src={artist} alt='artist graphic'/><h2>Artist</h2></Link></div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/components/Programer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Programer = () => {
+  return (
+    <div>Programer</div>
+  )
+}
+
+export default Programer
