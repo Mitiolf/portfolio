f2215ab74ce4875239ad5529790b82579fc1985c
Wojtek <mitiolf@outlook.com>
2022-03-31 12:55:13 +0200

    text

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -15,7 +15,11 @@ const Artist = () => {
     </section>
     <section className='about-skils'>
       <div className='about'><h2>About</h2>
-        <p>I'm a student of social computer sciense at AGH in Cracow. I started my programing journey half year ago. Before I wrote some code in pyton on my studies. I'm a student of social computer sciense at AGH in Cracow. I started my programing journey half year ago. Before I wrote some code in pyton on my studies.</p>
+        <p>I'm interested in every kind of art. Currently, I'm focusing on dance because it includes music and movement.
+        Music is important to me because I attended the Wadowice Music School, where I learned to play bassoon and piano.
+        Movement also plays a key role in my life. I have been playing sports since I can remember.
+        I'm fascinated by the human body and nature. I love hiking and being in the wild.
+        </p>
       </div>
       <div className='skils'><h2>Skils</h2>
         <ul><li>Playing piano</li><li>Drawing</li><li>Inkscape</li><li>Figma</li><li>Contemporary Dance</li><li>Movement</li></ul>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
     <div className='homepage-container'>
         <h1>Hi, I'm Wojtek</h1>
         <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2>Programer</h2></Link></div>
-        <div className='artist-container'><Link to="artist"><img src={artist} alt='artist graphic'/><h2>Artist</h2></Link></div>
+        <div className='artist-container'><Link to="/artist"><img src={artist} alt='artist graphic'/><h2>Artist</h2></Link></div>
     </div>
   )
 }
