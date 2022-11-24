8931e26c0519e8eb3d17fd0a5b7d37ac747c445b
Wojtek <mitiolf@outlook.com>
2022-11-24 10:34:22 +0100

    Brain

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,8 +6,8 @@ const Homepage = () => {
   return (
     <div className='homepage-container'>
         <h1>Hi, I'm Wojtek</h1>
-        <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2>Programer</h2></Link></div>
-        <div className='artist-container'><Link to="/artist"><img src={artist} alt='artist graphic'/><h2>Artist</h2></Link></div>
+        <div className='programer-container'><Link to="/programer"><img src={programer} alt="programer graphic"/><h2>Left Brain</h2></Link></div>
+        <div className='artist-container'><Link to="/artist"><img src={artist} alt='artist graphic'/><h2>Right Brain</h2></Link></div>
     </div>
   )
 }
